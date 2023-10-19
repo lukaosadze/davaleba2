a6b36a1cac2325e81ca026f34d0eb7ddee967185
luka2004 <lukaosadze376@gmail.com>
2023-10-19 14:05:29 +0400

    Initial commit

---
+++ javascrift/script.js
@@ -0,0 +1,16 @@
+alert("hellow world");
+
+
+function firstfunction(luka , saba){
+    console.log("function");
+   
+
+  if(firstfunction < 10){
+        alert("metia")
+
+        }else{;  
+    }
+    
+ }
+       
+    
